cd1088ebef7e5cb043c90974bf32331e8be63ee3
saymtech <mbessonnier@saymtech.fr>
2017-03-08 13:17:47 +0100

    Erreur d'identifiant

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "tobeca"
+plugin_identifier = "tobecaplugin"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
 plugin_package = "octoprint_%s" % plugin_identifier
